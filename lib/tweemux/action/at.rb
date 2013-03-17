fff9a8e1ddce449f2c3885946195ca02e427b072
☈king <rking@sharpsaw.org>
2013-03-16 19:08:50 -0500

    'On' is now the primary, 'at' the alias

--- lib/tweemux/action/at.rb
+++ lib/tweemux/action/at.rb
@@ -1,25 +1,2 @@
-class Tweemux::Action::At < Tweemux::Action
-  def run args
-    host, port, *rest = args
-    cmd = 'ssh', host
-    if port
-      cmd.push '-p'+port
-      port_parens = " (on port #{port})"
-    end
-    desc = ["Connect to #{host}#{port_parens}"]
-    forced_path_for_osx_people = 'PATH=/usr/local/bin:/usr/bin'
-    cmd.concat [ '-t', forced_path_for_osx_people ]
-    desc.push 'demand a pty'
-    cmd.concat [ 'tmux', '-2uS', Tweemux::SOCK ]
-    desc.push 'Run tmux (with 256 colors, UTF-8, on the shared socket'
-    if rest.empty?
-      cmd.push 'attach'
-      desc.push 'attach to existing session'
-    else
-      cmd.concat rest
-      desc.push rest.join ' '
-    end
-    description = desc.join ', '
-    explained_run cmd, description
-  end
-end
+require 'tweemux/action/on'
+class Tweemux::Action::At < Tweemux::Action::On; end
--- lib/tweemux/action/on.rb
+++ lib/tweemux/action/on.rb
@@ -1,1 +1,25 @@
-class Tweemux::Action::On < Tweemux::Action::At; end
+class Tweemux::Action::On < Tweemux::Action
+  def run args
+    host, port, *rest = args
+    cmd = 'ssh', host
+    if port
+      cmd.push '-p'+port
+      port_parens = " (on port #{port})"
+    end
+    desc = ["Connect to #{host}#{port_parens}"]
+    forced_path_for_osx_people = 'PATH=/usr/local/bin:/usr/bin'
+    cmd.concat [ '-t', forced_path_for_osx_people ]
+    desc.push 'demand a pty'
+    cmd.concat [ 'tmux', '-2uS', Tweemux::SOCK ]
+    desc.push 'Run tmux (with 256 colors, UTF-8, on the shared socket'
+    if rest.empty?
+      cmd.push 'attach'
+      desc.push 'attach to existing session'
+    else
+      cmd.concat rest
+      desc.push rest.join ' '
+    end
+    description = desc.join ', '
+    explained_run cmd, description
+  end
+end
