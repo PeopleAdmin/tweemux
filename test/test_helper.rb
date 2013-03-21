a6f6beccf41d2341b4b1a2e7b7abf49f22d03958
☈king <parking@sharpsaw.org>
2013-03-21 07:23:09 -0500

    Make this work in or out of Spork

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,15 @@
 # Run this via 'beg'
 require 'working/test_helper'
 
-require 'tweemux'
-
-Spork.each_run do
+def reload
   Dir['lib/**/*.rb'].each{|e| load e}
+end
+
+if defined? Spork
+  Spork.each_run do
+    reload
+  end
+else
+  reload
 end
 # Spork.prefork doesn't like when this is missing
