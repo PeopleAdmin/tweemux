e684e383eb56bc388841e82c37ff57179d9edf55
☈king <parking@sharpsaw.org>
2013-03-21 07:24:13 -0500

    Remove sham impl of 'help'; close #32

--- lib/tweemux/action/help.rb
+++ lib/tweemux/action/help.rb
@@ -2,7 +2,7 @@ class Tweemux::Action::Help < Tweemux::Action
   def run _
     five_dirs_up = (['..']*5).join '/'
     readme_path =
-      File.expand_path 'lib/tweemux/action/help.rb'+five_dirs_up+'/README.md'
+      File.expand_path __FILE__+five_dirs_up+'/README.md'
     contents = File.read readme_path
     contents.sub! /^.+(?=## Guest Usage)/m, ''
     contents.sub! /## Going Further.*/m, ''
